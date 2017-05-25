fb281479526aecec6230e602d83208a33a9ce5e9
U140172 <U140172@54006p21.ds.upf.edu>
2017-05-25 12:50:37 +0200

    First Commit. Project Init

---
+++ main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: U140172
+ *
+ * Created on 25 de mayo de 2017, 12:48
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
